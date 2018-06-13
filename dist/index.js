d05d7927f4df72d41b4afdc61684a9b38d0ffadd
suwigyarathore <Suwi.Rathore@klm.com>
2018-06-13 21:39:15 +0200

    Addin basic styling

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,14 @@
 const { createElement } = React;
 const { render } = ReactDOM;
+
+const style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'verdana'
+}
 const title = createElement(
   'h1',
-  { id: 'title', className: 'headeer' },
+  { id: 'title', className: 'headeer', style: style },
   'Hello world'
 );
 
