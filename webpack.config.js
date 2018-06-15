695145771cd0632c155924dc57ef5fc2dbe266cd
suwigyarathore <Suwi.Rathore@klm.com>
2018-06-15 08:40:35 +0200

    Integrating sass loader and auto prefixer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,23 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
