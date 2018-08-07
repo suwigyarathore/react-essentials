fdcadddee97cb2395d48cb46358bc4f4b65b4b85
suwigyarathore <Suwi.Rathore@klm.com>
2018-08-07 09:38:46 +0200

    Addding props from outside

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { render } from "react-dom";
 import SkiDayCount from "./components/SkiDayCount";
 
-render(<SkiDayCount />,
+render(<SkiDayCount total={50}
+  powder={20}
+  backcountry={10}
+  goal={100} />,
   document.getElementById('react-container')
 )
