1110019ad4b0e48c58437534ff9d3337901ec509
suwigyarathore <Suwi.Rathore@klm.com>
2018-08-08 08:18:50 +0200

    Adding Static routes with React-Router V4 using Hash Router

---
+++ src/components/Whoops404.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Whoops404 extends Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          Whoops, route not found
+      </h1>
+      </div>
+    );
+  }
+}
+
+export default Whoops404;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 
 import React from 'react'
-import { render } from 'react-dom';
+import { render } from 'react-dom'
+import {
+  HashRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import App from './components/App'
+import Whoops404 from './components/Whoops404'
 
-render(<App />,
+render(<Router>
+  <Switch>
+    <Route exact path='/' component={App} />
+    {/* both /roster and /roster/:number begin with /roster */}
+    <Route path='*' component={Whoops404} />
+  </Switch>
+</Router>,
   document.getElementById('react-container')
 )
